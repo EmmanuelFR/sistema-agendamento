83bf66700a25c220baca565696effdb4b77672fc
EmmanuelFR <emmanuel.unifaa@gmail.com>
2024-10-31 13:38:13 -0300

    :sparkles: adicionando credenciais individuais para cada tipo de usuário.

--- src/main/java/com/sistema/agendamento/sistema_agendamento/controllers/LoginController.java
+++ src/main/java/com/sistema/agendamento/sistema_agendamento/controllers/LoginController.java
@@ -1,3 +1,25 @@
+// package com.sistema.agendamento.sistema_agendamento.controllers;
+
+// import org.springframework.http.HttpStatus;
+// import org.springframework.http.ResponseEntity;
+// import org.springframework.web.bind.annotation.*;
+// import com.sistema.agendamento.sistema_agendamento.dto.LoginRequest;
+
+// @RestController
+// @RequestMapping("/api")
+// @CrossOrigin(origins = "http://localhost:3000") 
+// public class LoginController {
+
+//     @PostMapping("/login")
+//     public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest) {
+//         if ("user@email.com".equals(loginRequest.getEmail()) && "2413".equals(loginRequest.getSenha())) {
+//             return ResponseEntity.ok("Login bem-sucedido");
+//         } else {
+//             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Credenciais inválidas");
+//         }
+//     }
+// }
+
 package com.sistema.agendamento.sistema_agendamento.controllers;
 
 import org.springframework.http.HttpStatus;
@@ -12,7 +34,16 @@ public class LoginController {
 
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest) {
-        if ("user@email.com".equals(loginRequest.getEmail()) && "2413".equals(loginRequest.getSenha())) {
+
+        String email = loginRequest.getEmail();
+        String senha = loginRequest.getSenha();
+        String tipoUsuario = loginRequest.getTipoUsuario();
+
+        if (tipoUsuario.equals("aluno") && email.equals("aluno@email.com") && senha.equals("al123")) {
+            return ResponseEntity.ok("Login bem-sucedido");
+        } else if (tipoUsuario.equals("polo") && email.equals("polo@email.com") && senha.equals("pl123")) {
+            return ResponseEntity.ok("Login bem-sucedido");
+        } else if (tipoUsuario.equals("administrador") && email.equals("admin@email.com") && senha.equals("ad123")) {
             return ResponseEntity.ok("Login bem-sucedido");
         } else {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Credenciais inválidas");
--- src/main/java/com/sistema/agendamento/sistema_agendamento/dto/LoginRequest.java
+++ src/main/java/com/sistema/agendamento/sistema_agendamento/dto/LoginRequest.java
@@ -1,15 +1,49 @@
+// package com.sistema.agendamento.sistema_agendamento.dto;
+
+// public class LoginRequest {
+//     private String email;
+//     private String senha;
+
+//     // Construtores
+//     public LoginRequest() {}
+
+//     public LoginRequest(String email, String senha) {
+//         this.email = email;
+//         this.senha = senha;
+//     }
+
+//     // Getters e Setters
+//     public String getEmail() {
+//         return email;
+//     }
+
+//     public void setEmail(String email) {
+//         this.email = email;
+//     }
+
+//     public String getSenha() {
+//         return senha;
+//     }
+
+//     public void setSenha(String senha) {
+//         this.senha = senha;
+//     }
+// }
+
 package com.sistema.agendamento.sistema_agendamento.dto;
 
 public class LoginRequest {
     private String email;
     private String senha;
+    private String tipoUsuario;
 
     // Construtores
     public LoginRequest() {}
 
-    public LoginRequest(String email, String senha) {
+    public LoginRequest(String email, String senha, String tipoUsuario) {
         this.email = email;
         this.senha = senha;
+        this.tipoUsuario = tipoUsuario;
     }
 
     // Getters e Setters
@@ -27,5 +61,13 @@ public class LoginRequest {
 
     public void setSenha(String senha) {
         this.senha = senha;
+    }
+
+    public String getTipoUsuario() {
+        return tipoUsuario;
+    }
+
+    public void setTipoUsuario(String tipoUsuario) {
+        this.tipoUsuario = tipoUsuario;
     }
 }
