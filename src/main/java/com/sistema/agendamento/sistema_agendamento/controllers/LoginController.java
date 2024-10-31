89db6775c90aedc108e56622c43674ab2018e580
EmmanuelFR <emmanuel.unifaa@gmail.com>
2024-10-31 19:09:57 -0300

    :sparkles: Limpeza geral atualizada.

--- src/main/java/com/sistema/agendamento/sistema_agendamento/controllers/LoginController.java
+++ src/main/java/com/sistema/agendamento/sistema_agendamento/controllers/LoginController.java
@@ -1,25 +1,3 @@
-// package com.sistema.agendamento.sistema_agendamento.controllers;
-
-// import org.springframework.http.HttpStatus;
-// import org.springframework.http.ResponseEntity;
-// import org.springframework.web.bind.annotation.*;
-// import com.sistema.agendamento.sistema_agendamento.dto.LoginRequest;
-
-// @RestController
-// @RequestMapping("/api")
-// @CrossOrigin(origins = "http://localhost:3000") 
-// public class LoginController {
-
-//     @PostMapping("/login")
-//     public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest) {
-//         if ("user@email.com".equals(loginRequest.getEmail()) && "2413".equals(loginRequest.getSenha())) {
-//             return ResponseEntity.ok("Login bem-sucedido");
-//         } else {
-//             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Credenciais inválidas");
-//         }
-//     }
-// }
-
 package com.sistema.agendamento.sistema_agendamento.controllers;
 
 import org.springframework.http.HttpStatus;
--- src/main/java/com/sistema/agendamento/sistema_agendamento/dto/LoginRequest.java
+++ src/main/java/com/sistema/agendamento/sistema_agendamento/dto/LoginRequest.java
@@ -1,35 +1,3 @@
-// package com.sistema.agendamento.sistema_agendamento.dto;
-
-// public class LoginRequest {
-//     private String email;
-//     private String senha;
-
-//     // Construtores
-//     public LoginRequest() {}
-
-//     public LoginRequest(String email, String senha) {
-//         this.email = email;
-//         this.senha = senha;
-//     }
-
-//     // Getters e Setters
-//     public String getEmail() {
-//         return email;
-//     }
-
-//     public void setEmail(String email) {
-//         this.email = email;
-//     }
-
-//     public String getSenha() {
-//         return senha;
-//     }
-
-//     public void setSenha(String senha) {
-//         this.senha = senha;
-//     }
-// }
-
 package com.sistema.agendamento.sistema_agendamento.dto;
 
 public class LoginRequest {
